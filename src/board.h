b73b6af17d9c7699b41429c30f682bb5867b05c6
euclidthedark <euclidthedark@gmail.com>
2020-07-26 15:00:12 -0400

    Add coordinate system to game_board struct.

--- src/board.h
+++ src/board.h
@@ -1,6 +1,8 @@
 struct Board {
   int width;
   int height;
+  int y_offset;
+  int x_offset;
 };
 
 typedef struct Board Board;
--- src/main.c
+++ src/main.c
@@ -3,11 +3,16 @@
 
 int main()
 {
-  Board game_board = {200, 55};
+  Board game_board = {200, 55, 2, 10};
 
   initscr();
   curs_set(0);
-  WINDOW *win = newwin(game_board.height, game_board.width, 2, 10);
+  WINDOW *win = newwin(
+    game_board.height,
+    game_board.width,
+    game_board.y_offset,
+    game_board.x_offset
+  );
   refresh();
   box(win, 0, 0);
   wrefresh(win);
